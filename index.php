b6216fcad7abf54db290d617c0d9ee1e7a20e49c
zhuyoubing <zhu.youbing@zhimatech.com>
2014-12-26 14:06:19 +0800

    update example

--- index.php
+++ index.php
@@ -10,12 +10,11 @@ use Avos\AVUser as AVUser;
 use Avos\AVException as AVException;
 
 session_start();
-AVClient::initialize("10ono71kr81a8cp1kvskbnqev6ryr1ogccu3cmwheungsw71",
-    "24rlcmvk2hx2zqi8nehtv7f0lm6ej4vl90thkbxrb4ssme3q", "l98q00w0w4nfdj9h0c9t8fzsc09g3lca4vhqjf9g9qxuwr0s");
+AVClient::initialize('your_app_id', 'your_app_key', 'your_master_key');
 AVClient::setStorage(new AVSessionStorage());
 
 try {
-    AVUser::logIn('18014828927','12344444');
+    AVUser::logIn('username','password');
     echo "login";
 } catch (AVException $ex) {
 	var_dump($ex->getMessage());
