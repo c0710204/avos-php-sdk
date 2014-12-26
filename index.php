b7610d00d1ec821f72428540c222f60041bfd96f
zhuyoubing <zhu.youbing@zhimatech.com>
2014-12-26 15:42:46 +0800

    update example

--- index.php
+++ index.php
@@ -8,16 +8,24 @@ use Avos\AVClient as AVClient;
 use Avos\AVSessionStorage as AVSessionStorage;
 use Avos\AVUser as AVUser;
 use Avos\AVException as AVException;
+use Avos\AVObject as AVObject;
 
 session_start();
-AVClient::initialize('your_app_id', 'your_app_key', 'your_master_key');
+AVClient::initialize("app_id","rest_key", "master_key");
 AVClient::setStorage(new AVSessionStorage());
 
-try
-{
-    AVUser::logIn('username', 'password');
-    echo "login";
-} catch (AVException $ex)
-{
-    var_dump($ex->getMessage());
-}
+$object = AVObject::create("TestObject");
+$objectId = $object->getObjectId();
+$php = $object->get("elephant");
+
+// Set values:
+$object->set("elephant", "php");
+$object->set("today", new DateTime());
+$object->setArray("mylist", [1, 2, 3]);
+$object->setAssociativeArray(
+  "languageTypes", array("php" => "awesome", "ruby" => "wtf")
+);
+
+// Save:
+$object->save();
+echo "saved object id:".$object->getObjectId();
